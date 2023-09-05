3d0d7266663b0ee41adbb5dc251aa5c5776e2e37
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2023-09-04 23:49:09 -0400

    added cow + duck info !

--- src/lib/data/cows.ts
+++ src/lib/data/cows.ts
@@ -4,10 +4,59 @@ import type Cow from "$lib/models/cow.model";
 const COWS: Cow[] = [
 	{
 		id: 1,
-		name: "Cheerio Catapult",
-		imageURL: "/images/cows/1.png",
+		name: "Cowapult",
+		imageURL: "/images/cows/cowapult.png",
 		description: "A cow that launches cheerios up at ducks.",
-		cost: 25,
+		cost: 200,
+	},
+	{
+		id: 2,
+		name: "Wheat Crop",
+		imageURL: "/images/cows/wheat_crop.png",
+		description: "This one isn't a cow. You can't make Cheerios without wheat!",
+		cost: 100
+	},
+	{
+		id: 3,
+		name: "Cereal Bodyguard",
+		imageURL: "/images/cows/cereal_box_cow.png",
+		description: "A peaceful cow willing to protect the Cheerios at all costs. Has exceptional health. His dry cleaner hates him.",
+		cost: 200
+	},
+	{
+		id: 4,
+		name: "Cow Cannon",
+		imageURL: "/images/cows/cow_cannon.png",
+		description: "Shoots rainbow cheerios that can hit multiple ducks.",
+		cost: 400
+	},
+	{
+		id: 5,
+		name: "Cheerio Bomb",
+		imageURL: "/images/cows/cheerio_bomb.png",
+		description: "Explodes upon placement in a puff of cheeerio-goodness.",
+		cost: 500
+	},
+	{
+		id: 6,
+		name: "We Cow Cows",
+		imageURL: "/images/cows/stacked_cow.png",
+		description: "A crowd of cows that like standing on each other.",
+		cost: 175
+	},
+	{
+		id: 7,
+		name: "Chilled Cheerios",
+		imageURL: "/images/cows/chilled_cereal.png",
+		description: "Specially prepared cheerios that slow down ducks.",
+		cost: 300,
+	},
+	{
+		id: 8,
+		name: "Spilt Cereal",
+		imageURL: "/images/cows/spilt_cereal.png",
+		description: "No use crying over spilt cereal. Damages ducks that step on it. Dies on contact with rubber duck.",
+		cost: 200
 	}
 ]
 
--- src/lib/data/ducks.ts
+++ src/lib/data/ducks.ts
@@ -5,9 +5,32 @@ const DUCKS: Duck[] = [
 	{
 		id: 1,
 		name: "Basic Duck",
-		imageURL: "/images/ducks/1.png",
+		imageURL: "/images/ducks/basic_duck.png",
 		description: "A basic duck that loves being a duck and hates cows with a passion.",
-		
+	},
+	{
+		id: 2,
+		name: "Duck with (butter) Knife",
+		imageURL: "/images/ducks/knife_duck.png",
+		description: "Knife for decorative purposes decorative purposes only. Attack power comes from the heart."
+	},
+	{
+		id: 3,
+		name: "Pond Duck",
+		imageURL: "/images/ducks/river_duck.png",
+		description: "Pond life has strengthened this duck."
+	},
+	{
+		id: 4,
+		name: "Gargantuan Duck",
+		imageURL: "/images/ducks/basic_duck.png",
+		description: "A basic duck that grew up drinking milk and eating all their veggies."
+	},
+	{
+		id: 5,
+		name: "Rubber Duck",
+		imageURL: "/images/ducks/rubber_duck.png",
+		description: "Duck native to the waters of Toronto. Dies on contact with Spilt Cereal."
 	}
 ]
 
